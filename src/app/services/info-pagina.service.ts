6235ab4799e16c76ea0909a12ac98c69e75b4dce
Gabrieljf217 <gabrieljf97@hotmail.com>
2019-02-27 22:40:50 -0500

    procustos cargados desde firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,20 +9,33 @@ export class InfoPaginaService {
 
     info:InfoPagina={};
     cargada= false;
+    equipo:any[]=[];
 
   constructor( private http: HttpClient ) { 
 
-    console.log('servicio de infoPagina listo');
+      this.cargarInfo();
+      this.cargarEquipo();
 
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina)=>{
         
         this.cargada=true;
         this.info=resp;
-        console.log(resp);
 
-      } )
+      });
+  }
 
+  private cargarEquipo() {
+    
+    this.http.get('https://angular-html-bb189.firebaseio.com/equipo.json')
+    .subscribe( (resp:any[])=>{
+      
+      this.equipo=resp;
 
-    }
+    });
+  }
+
 }
